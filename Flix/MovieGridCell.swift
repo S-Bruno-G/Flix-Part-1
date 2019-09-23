fcb01e94472e3e2fa61861086ebd88753ecb6fa1
B34ST9 <b34st9@B34ST9s-MacBook-Air.local>
2019-09-23 16:53:50 -0700

    adding Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by B34ST9 on 9/23/19.
+//  Copyright © 2019 Santiago Bruno. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
